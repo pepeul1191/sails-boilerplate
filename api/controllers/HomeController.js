d59e66e8578fa2521b02d82a93051576016d0311
Pepe Valdivia <jvaldivia@softweb.pe>
2019-07-21 20:56:28 -0500

    Primer controlador

---
+++ api/controllers/HomeController.js
@@ -0,0 +1,14 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  async index(req, res) {
+    sails.log.debug('TODO: implement???');
+    return res.ok();
+  }, 
+};
+
